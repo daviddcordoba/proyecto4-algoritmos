45ce86491173b9cb03957e2f88af257224df21e7
David <cordoba_d@rocketmail.com>
2022-10-31 09:49:06 -0300

    terminade

--- 5/arreglos_entrada_salida.c
+++ 5/arreglos_entrada_salida.c
@@ -2,16 +2,33 @@
 #include <assert.h>
 
 void pedirArrreglo(int a[], int n_max){
-
+    int i = 0;
+    while(i<n_max){
+        printf("Ingrese el elemento %d del arreglo: ",i);
+        scanf("%d",&a[i]);
+        i++;
+    }
 }
 
 void imprimeArreglo(int a[], int n_max){
-    
+    int i = 0;
+    printf("Arreglo: ");
+    while(i<n_max){
+        printf("%d ",a[i]);
+        i++;
+    }
 }
 
 
 int main(){
+    int tam;
+    printf("Ingrese el tamanio del arreglo: ");
+    scanf("%d",&tam);
 
+    int a[tam];
+
+    pedirArrreglo(a,tam);
+    imprimeArreglo(a,tam);
 
 
     return 0;
